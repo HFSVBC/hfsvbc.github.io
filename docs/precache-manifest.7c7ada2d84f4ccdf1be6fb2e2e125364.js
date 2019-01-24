8ea14195d3bd0d6582cfa218a20b2d2bec3a4bb0
Hugo Curado <h.curado@runtime-revolution.com>
2019-01-24 18:00:08 +0000

    Add post-build

---
+++ docs/precache-manifest.7c7ada2d84f4ccdf1be6fb2e2e125364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88e2da25230f94703ac",
+    "url": "/static/css/main.e1fb6cd6.chunk.css"
+  },
+  {
+    "revision": "c88e2da25230f94703ac",
+    "url": "/static/js/main.c88e2da2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb4b1fc57c92a62bb04e9f7c09e5bf03",
+    "url": "/index.html"
+  }
+];
