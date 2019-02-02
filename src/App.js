49e0a8dbdcd432f61a28273ad8f46c26d499d7b4
Hugo Curado <hfsvbc@hugocurado.info>
2019-02-02 12:56:04 +0000

    Build app after last commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { MainMenu } from "./components/main_menu";
 import { Header } from "./components/header";
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 import './App.css';
 
 class App extends Component {
